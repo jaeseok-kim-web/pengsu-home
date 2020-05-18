9764eb4673fb395dbd101fccff66c0556909167f
jaeseok-kim-web <jaequ0502@gmail.net>
2020-05-18 15:37:20 +0900

    웹페이지 생성

---
+++ js/main.js
@@ -0,0 +1,15 @@
+(function (window, document) {
+    'use strict'
+
+    const toggleBtn = document.querySelector('.header_toggleBtn');
+    const menu = document.querySelector('.header_menu');
+    const icons = document.querySelector('.header_icons');
+    
+
+    toggleBtn.addEventListener('click', () => {
+        menu.classList.toggle('active');
+        icons.classList.toggle('active');
+    });
+
+
+})(window, document)
